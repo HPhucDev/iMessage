a602260aea10e116a5d641edce46ecb36499fbae
HPhucDEV <hoangphucdeveloper@gmail.com>
2022-03-16 23:56:49 +0700

    Revert "Fix navbar box chat"
    This reverts commit 1cc1f8cc97511c51c2538f85520ebf7576c5a788.

--- app/src/main/java/com/example/imessage/Activities/ChatActivity.java
+++ app/src/main/java/com/example/imessage/Activities/ChatActivity.java
@@ -31,7 +31,7 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected  void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         binding =ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -96,7 +96,8 @@ public class ChatActivity extends AppCompatActivity {
                 });
             }
         });
-        binding.nameView.setText(""+name);
+        //binding.nameView.setText(""+name);
+        getSupportActionBar().setTitle(name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
